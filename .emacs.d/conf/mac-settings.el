14d840910e108df6d98972e87504fb037854b4eb
Yuta Kato <yyttkktt@gmail.com>
2012-07-16 02:39:45 +0900

    rm tar file

--- .emacs.d/conf/mac-settings.el
+++
@@ -1,8 +0,0 @@
-;; Macだけに読み込ませる内容
-(when (eq system-type 'darwin)
-  ;; MacのEmacsでファイル名を正しく扱うための設定
-  (require 'ucs-normalize)
-  (setq file-name-coding-system 'utf-8-hfs)
-  (setq locale-coding-system 'utf-8-hfs))
-
-
