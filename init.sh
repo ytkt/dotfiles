03bbd6e31015854c0d85de1235bd849a7b8e5a6d
Yuta Kato <yyttkktt@gmail.com>
2013-10-06 02:55:58 +0900

    pryrcを追加

--- init.sh
+++ init.sh
@@ -5,4 +5,5 @@ ln -sf $HOME/dotfiles/.bash_profile $HOME/
 ln -sf $HOME/dotfiles/.emacs.d $HOME/
 ln -sf $HOME/dotfiles/git/.gitignore $HOME/
 ln -sf $HOME/dotfiles/git/.gitconfig $HOME/
+ln -sf $HOME/dotfiles/.pryrc $HOME/
 #ln -sf .zshrc
